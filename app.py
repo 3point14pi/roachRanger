d5c688abde3ff7f954c3389e95e3d9d0d457068e
pyxkam <kamalashree.s@pyxeda.ai>
2025-03-10 06:29:13 +0530

    implemented fixes for indentatio error

--- app.py
+++ app.py
@@ -5,16 +5,20 @@ from model_utils import load_yolo_model
 import os
 import shutil
 
-IMAGE_ADDRESS = "https://i.ytimg.com/vi/bEwCA_nrY5Q/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLB4y6YJGxw6oVxFm-Uucbrjfqhu2Q"
+
+# constants
 IMAGE_NAME = "uploaded.png"
+IMAGE_ADDRESS = "https://i.ytimg.com/vi/bEwCA_nrY5Q/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLB4y6YJGxw6oVxFm-Uucbrjfqhu2Q"
 PRED_IMAGE_PATH = "runs/detect/predict/uploaded.png"
 DIRECTORY = "runs/detect/predict"
 PRED_MOVE_NAME = "pred_image.png"
+
+# load the yolo model
 yolo_model = load_yolo_model()
 
-def make_predictions(IMAGE_PATH):
-  results = yolo_model.predict(IMAGE_PATH, save=True, conf=0.75)
-  try:
+def make_predictions(image_path):
+    results = yolo_model.predict(image_path, save = True, conf = 0.5)
+    try:
         if os.path.exists(PRED_IMAGE_PATH):
             shutil.move(PRED_IMAGE_PATH, PRED_MOVE_NAME)
             os.rmdir(DIRECTORY)
@@ -26,11 +30,16 @@ def make_predictions(IMAGE_PATH):
         print(str(error))
 
         return False
-  
-st.title("Cockroach Detection")
-st.image(IMAGE_ADDRESS, caption = "Cockroach Detection"
 
+
+# UI
+st.title("Cockroach Detection")
+st.image(IMAGE_ADDRESS, caption = "Cockroach Detection")
+
+# File uploader widget
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+# Check if a file has been uploaded
 if uploaded_file is not None:
     # Open the uploaded file using PIL
     image = Image.open(uploaded_file)
